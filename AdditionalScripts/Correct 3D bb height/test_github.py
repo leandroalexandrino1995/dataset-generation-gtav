f01a68b2e7824c5ab98c7d0b1de90c6334c3f586
João Gomes <j.gomes@ua.pt>
2021-07-28 16:46:09 +0100

    Correct 3D bb to have the same height as the 2D bb

--- AdditionalScripts/Correct 3D bb height/test_github.py
+++ AdditionalScripts/Correct 3D bb height/test_github.py
@@ -1,59 +1,19 @@
-"""
-Created on Sat Mar 24 12:38:17 2018
-The KITTI is one of the well known benchmarks for 3D Object detection. Working 
-on this dataset requires some understanding of what the different files and their
-contnts are. In this piece we use  4 different types of files used from the 
-KITTI 3D Objection Detection dataset as follows to do some basic manipulation 
-and sanity checks to get basic underdstanding. 
- camera2 image (.png), 
- camera2 label label (.txt),
- calibration (.txt), 
- velodyne point cloud (.bin),
-Codes to project 3D data from  camera co-ordinate and velodyne coordinate to  
-camera image.  The goal is to see if the  data along with appropriate geometry 
-matrices are handled correctly.  2 different types of images are generated - 
-camera2 image and bird's eye view of point cloud.  
-@author: sg
-Refs :
-  1. Vision meets Robotics: The KITTI Dataset - http://www.cvlibs.net/publications/Geiger2013IJRR.pdf
-  2. 3D Object Detection Evaluation 2017 - http://www.cvlibs.net/datasets/kitti/eval_object.php?obj_benchmark=3d
-  3. Download left color images of object data set (12 GB) - http://www.cvlibs.net/download.php?file=data_object_image_2.zip
-  4. Download Velodyne point clouds, if you want to use laser information (29 GB) - http://www.cvlibs.net/download.php?file=data_object_velodyne.zip
-  5. Download camera calibration matrices of object data set (16 MB) - http://www.cvlibs.net/download.php?file=data_object_calib.zip
-  6. Download training labels of object data set (5 MB) - http://www.cvlibs.net/download.php?file=data_object_label_2.zip
-  7. Download object development kit (1 MB) (including 3D object detection and bird's eye view evaluation code) - http://kitti.is.tue.mpg.de/kitti/devkit_object.zip
-  
-"""
-
 import os
 import os.path
 import matplotlib.pyplot as plt
 import matplotlib.patches as patches
 from   matplotlib.path import Path
-from matplotlib import colors
 import numpy as np
 from PIL import Image
 from math import sin, cos
-import argparse
 
-from numpy.testing._private.utils import clear_and_catch_warnings
-
-
-# basedir = 'C:/data/kitti/data_small/training' # windows
 basedir = '../../../MaterialHash_noBackground/GTAtoKITTI/kitti/training/'
-# basedir = '../../../KITTI/training/'
-# basedir = '../../../Diogo/training/'
-# basedir = '../../../Diogo/kitti/training/'
-# basedir = 'data' # *nix
 left_cam_rgb= 'image_2'
 label = 'label_2'
 velodyne = 'velodyne'
 calib = 'calib'
 
 savedir = "../../../eyeball_test/all_train/"
-# savedir = "../../../KITTI/all_train/"
-# savedir = "../../../Diogo/all_train/"
-# savedir = "../../../Diogo/all_training/"
 
 
 def loadKittiFiles (frame) :
@@ -157,9 +117,7 @@ def computeBox3D(label, P, ratio=1):
   corners_2D = P.dot (corners_3D_1)
   corners_2D = corners_2D/corners_2D[2]
 
-  # edges, lines 3d/2d bounding box in vertex index 
-  edges = [[0,1], [1,2], [2,3], [3,4], [4,5], [5,6], [6,7], [7,0], [0,5], [1,4], [2,7], [3, 6]]
-  lines = [[0,1], [1,2], [2,3], [3,4], [4,5], [5,6], [6,7], [7,0], [0,5], [5, 4], [4, 1], [1,2], [2,7], [7,6], [6,3]]
+  # edges, lines 3d/2d bounding box in vertex index
   bb3d_lines_verts_idx = [0,1,2,3,4,5,6,7,0,5,4,1,2,7,6,3]
   
   bb2d_lines_verts = corners_2D[:,bb3d_lines_verts_idx] # 
@@ -168,7 +126,7 @@ def computeBox3D(label, P, ratio=1):
   aux = bb2d_lines_verts[1]
   ratio = max(aux)-min(aux)
    
-  return corners_2D[:2], corners_3D, bb2d_lines_verts[:2], ratio, h
+  return bb2d_lines_verts[:2], ratio, h
   
   
   
@@ -262,8 +220,8 @@ def labelToBoundingBox(ax, labeld, calibd):
    
       if key != 'DontCare' :
         
-        corners_2D, corners_3D, paths_2D, ratio, h = computeBox3D(labeld[key][o], P2_rect)
-        corners_2D, corners_3D, paths_2D, ratio, h = computeBox3D(labeld[key][o], P2_rect, height/ratio)
+        paths_2D, ratio, h = computeBox3D(labeld[key][o], P2_rect)
+        paths_2D, ratio, h = computeBox3D(labeld[key][o], P2_rect, height/ratio)
         verts = paths_2D.T # corners_2D.T
         codes = [Path.LINETO]*verts.shape[0]
         codes[0] = Path.MOVETO
@@ -271,52 +229,8 @@ def labelToBoundingBox(ax, labeld, calibd):
         p = patches.PathPatch( pth, fill=False, color='purple', linewidth=2)
         ax.add_patch(p)
         hs.append(h)
-        
-        
   
-  # a sanity test point in velodyne co-ordinate to check  camera2 imaging plane projection
-#   testp = [ 11.3, -2.95, -1.0]
-#   bb3d.append(testp)
-  
-#   xnd = np.array(testp+[1.0])
-#   #print ('bb3d xnd velodyne   ', xnd)
-#   #xpnd = P2.dot(R0_rect.dot(Tr_velo_to_cam.dot(xnd)))
-#   xpnd = Tr_velo_to_cam.dot(xnd)
-#   #print ('bb3d xpnd cam0      ', xpnd)
-#   xpnd = R0_rect.dot(xpnd)
-#   #print ('bb3d xpnd rect cam0 ', xpnd)
-#   xpnd = P2_rect.dot(xpnd)
-#   #print ('bb3d xpnd cam2 image', xpnd)
-#   #print ('bb3d xpnd cam2 image', xpnd/xpnd[2])
-  
-#   p = patches.Circle( (xpnd[0]/xpnd[2], xpnd[1]/xpnd[2]), fill=False, radius=3, color='red', linewidth=2)
-#   ax.add_patch(p)
-  
-  return np.array(bb2d), np.array(bb3d), corners_3D, hs
-
-def pointCloudToBirdsEyeView(ax2, velo, bb3d):
-  ax2.set_xlim (-10,10)
-  ax2.set_ylim (-5,35)
-  hmax = velo[:,2].max()
-  hmin = velo[:,2].min()
-  hmean = velo[:, 2].mean()
-  hmeadian = np.median ( velo[:, 2] )
-  hstd = np.std(velo[:, 2])
-  #print ('scalledh', hmax, hmean, hmeadian, hmin, hstd, scalledh.shape, scalledh[:10])
-  norm = colors.Normalize(hmean-2*hstd, hmean+2*hstd, clip=True)
-  sc2= ax2.scatter(-velo[:,1],
-             velo[:,0],
-             s = 1,
-             c=velo[:,2],
-             cmap = 'viridis',
-             norm=norm,
-             marker = ".",
-             )
-  ax2.scatter(-bb3d[:,1],
-             bb3d[:,0],
-             c='red')
-  ax2.set_facecolor('xkcd:grey')
-  plt.colorbar(sc2)
+  return hs
 
 def main ():
   """
@@ -341,7 +255,7 @@ def main ():
       ax.imshow(left_cam)
       
       
-      bb2d, bb3d, corners3d, hs = labelToBoundingBox(ax, label_data, calib_data)
+      hs = labelToBoundingBox(ax, label_data, calib_data)
 
       print(hs)
 
@@ -362,42 +276,9 @@ def main ():
                   f.write(i.split(" ")[x])
               else:
                 f.write(str(hs[aux])+" ")
+            aux += 1
           else:
             f.write(i)
-
-          aux += 1
-
-      
-      # return
-
-      plt.title(frame)
-      plt.savefig(savedir+"/"+frame+"_withBB_github.png")
-
-      break
-
-
-      # plt.savefig("/home/joao/Desktop/Diogo/"+frame+"_withBB_github.png")
-      
-      # point cloud to bird's eye view scatter plot
-      # ax2 = f.add_subplot(3,1,2, )#projection="3d" )
-      # pointCloudToBirdsEyeView(ax2, velo, bb3d)
-
-      to_write = "ply\nformat ascii 1.0\nelement vertex " + str(velo.shape[0]+corners3d.shape[1]) +  "\nproperty float x\nproperty float y\nproperty float z\nproperty float intensity\nend_header\n"
-      
-      x = velo[:,0]
-      y = velo[:,1]
-      z = velo[:,2]
-      r = velo[:,3]
-
-      for i in range(velo.shape[0]):
-          to_write += str(x[i]) + " " + str(y[i]) + " " + str(z[i]) + " " + str(r[i]) + "\n"
-
-      for i in range(corners3d.shape[1]):
-          to_write += str(corners3d[0,i]) + " " + str(corners3d[1,i]) + " " + str(corners3d[2,i]) + " 1\n"
-
-      with open(savedir+"/"+frame+"_github.ply", "w") as f:
-      # with open("/home/joao/Desktop/Diogo/"+frame+"_github.ply", "w") as f:
-          f.write(to_write)
 
 
 if __name__ == '__main__':
